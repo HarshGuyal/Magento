1847db2843020fb51dd9801af387ce5d974e73a7
harsh <harsh@guyal.com>
2024-12-14 17:05:24 +0530

    DONE

---
+++ app/code/Harsh/JsFun/registration.php
@@ -0,0 +1,8 @@
+<?php 
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    type:ComponentRegistrar::MODULE,
+    componentName: 'Harsh_JsFun',
+    path:__DIR__
+);
