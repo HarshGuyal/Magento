c4dcf6060e3ac2820cd91c55355e94deb8995fa4
harsh <harsh@guyal.com>
2024-12-16 17:48:45 +0530

    JAVASCRIPT

---
+++ app/code/Harsh/JsFun/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    "map" : {
+        "*" : {
+            "fadeInElement" : "Harsh_JsFun/js/fade-in-element"
+        }
+    },
+    "paths" : {
+        "vue" : "https://cdn.jsdelivr.net/npm/vue@2/dist/vue"
+    }
+}   
