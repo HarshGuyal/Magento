a49199d02a8976c8eb1a2024ae4fa44fb66d508d
harsh <harsh@guyal.com>
2024-12-23 16:31:39 +0530

    KNOCKOUTUI

---
+++ app/code/Harsh/InventoryFulfillment/view/frontend/web/js/box-configurations.js
@@ -0,0 +1,40 @@
+define([
+    'uiComponent',
+    'ko'
+],function(
+    Component,
+    ko
+){
+    'use strict';
+
+    const boxConfiguration = () => {
+        return {
+            length: ko.observable(),
+            width: ko.observable(),
+            height: ko.observable(),
+            weight: ko.observable(),
+            unitsPerBox: ko.observable(),
+            numberOfBoxes: ko.observable(),
+        };
+    };
+
+    return Component.extend({
+        defaults: {
+            boxConfigurations: ko.observableArray([boxConfiguration()])
+        },
+        initialize(){
+            this._super();
+
+            console.log('the boxConfigurations component has been loaded. ')
+        },
+        handleAdd() {
+            this.boxConfigurations.push(boxConfiguration());
+        },
+        handleDelete(index) {
+            this.boxConfigurations.splice(index, 1);
+        },
+        handleSubmit() {
+            console.log('Submitted box configuration form.');
+        }
+    });
+});
--- app/code/Harsh/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
+++ app/code/Harsh/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
@@ -1,23 +1,46 @@
 define([
     'uiComponent',
-    'ko'
+    'ko',
+    'mage/storage',
+    'jquery',
+    'mage/translate'
 ], function(
     Component,
-    ko
+    ko,
+    storage,
+    $,
+    $t
 ) {
     'use strict';
     return Component.extend({
         defaults:{
             // template : 'Harsh_InventoryFulfillment/sku-lookup',
-            sku: ko.observable('ABC123'),
-            placeholder: 'Example: 24-MB01'
+            sku: ko.observable('24-MB01'),
+            placeholder: $t('Example: %1').replace('%1', '24-MB01'),
+            messageResponse: ko.observable(''),
+            isSuccess: ko.observable(false)
         },
         initialize() {
             this._super();
             console.log('The skuLookup component has been loaded.');
         },
         handleSubmit(){
-            console.log(this.sku() + 'SKU confirmed.')
+            $('body').trigger('processStart');
+            this.messageResponse('');
+            this.isSuccess(false);
+
+            storage.get(`rest/V1/products/${this.sku()}`)
+                .done(response => {
+                    this.messageResponse($t('Product found! %1').replace('%1', `<strong>${response.name}</strong>`));
+                    this.isSuccess(true);
+                }
+            ).fail(() => {
+                this.messageResponse($t('Product not found.'));
+                this.isSuccess(false);
+            })
+            .always(() => {
+                $('body').trigger('processStop');
+            });
         }
     });
 });
